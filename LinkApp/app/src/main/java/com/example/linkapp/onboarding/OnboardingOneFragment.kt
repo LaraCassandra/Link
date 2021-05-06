a07d94bb6997ce52cc936bb5ff56489999b890f8
LaraCassandra <190218@virtualwindow.co.za>
2021-05-06 12:54:40 +0200

    Begin onboarding screens

---
+++ LinkApp/app/src/main/java/com/example/linkapp/onboarding/OnboardingOneFragment.kt
@@ -0,0 +1,8 @@
+package com.example.linkapp.onboarding
+
+import androidx.fragment.app.Fragment
+import com.example.linkapp.R
+
+class OnboardingOneFragment : Fragment(R.layout.fragment_onboarding_one){
+
+}
