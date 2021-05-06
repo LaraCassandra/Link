53fa95affd9e2ce95d8fb5ba0e220d77f0708695
LaraCassandra <190218@virtualwindow.co.za>
2021-05-06 11:29:10 +0200

    Style main activity and login activity

--- LinkApp/app/src/main/java/com/example/linkapp/MainActivity.kt
+++ LinkApp/app/src/main/java/com/example/linkapp/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.linkapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // LOGIN BUTTON CLICK
+        val btn_login = findViewById<Button>(R.id.btn_login)
+        btn_login.setOnClickListener{
+            // DIRECT TO LOGIN PAGE
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
