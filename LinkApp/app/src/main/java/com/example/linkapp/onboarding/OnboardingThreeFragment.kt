3babaf9ef7a7a8d17ce427106a49c83bf90cc17d
LaraCassandra <190218@virtualwindow.co.za>
2021-05-06 13:05:48 +0200

    Style onboarding screens

---
+++ LinkApp/app/src/main/java/com/example/linkapp/onboarding/OnboardingThreeFragment.kt
@@ -0,0 +1,8 @@
+package com.example.linkapp.onboarding
+
+import androidx.fragment.app.Fragment
+import com.example.linkapp.R
+
+class OnboardingThreeFragment : Fragment(R.layout.fragment_onboarding_three){
+
+}
---
+++ LinkApp/app/src/main/java/com/example/linkapp/onboarding/OnboardingTwoFragment.kt
@@ -0,0 +1,8 @@
+package com.example.linkapp.onboarding
+
+import androidx.fragment.app.Fragment
+import com.example.linkapp.R
+
+class OnboardingTwoFragment : Fragment(R.layout.fragment_onboarding_two){
+
+}
