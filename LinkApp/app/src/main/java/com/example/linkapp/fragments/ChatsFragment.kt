67fd3fb7dd58729476c6e0d63255c61f7287cb73
LaraCassandra <190218@virtualwindow.co.za>
2021-05-13 14:34:17 +0200

    Add fragments for chats activity

---
+++ LinkApp/app/src/main/java/com/example/linkapp/fragments/ChatsFragment.kt
@@ -0,0 +1,8 @@
+package com.example.linkapp.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.linkapp.R
+
+class ChatsFragment : Fragment(R.layout.fragment_chats){
+
+}
---
+++ LinkApp/app/src/main/java/com/example/linkapp/fragments/ProfileFragment.kt
@@ -0,0 +1,8 @@
+package com.example.linkapp.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.linkapp.R
+
+class ProfileFragment : Fragment(R.layout.fragment_profile){
+
+}
