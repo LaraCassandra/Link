c7e37b9046ea681fe966f7d01e114f1a60c5372a
LaraCassandra <190218@virtualwindow.co.za>
2021-05-13 14:45:25 +0200

    Add functionality for functions

--- LinkApp/app/src/main/java/com/example/linkapp/ChatsActivity.kt
+++ LinkApp/app/src/main/java/com/example/linkapp/ChatsActivity.kt
@@ -3,6 +3,10 @@ package com.example.linkapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.linkapp.fragments.ChatsFragment
+import com.example.linkapp.fragments.ContactsFragment
+import com.example.linkapp.fragments.ProfileFragment
 import com.example.linkapp.model.User
 import com.example.linkapp.utils.Constants
 import com.example.linkapp.utils.Firestore
@@ -11,6 +15,51 @@ class ChatsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chats)
+
+        // REFERENCE TO FRAGMENTS
+        val chatsFragment = ChatsFragment()
+        val contactsFragment = ContactsFragment()
+        val profileFragment = ProfileFragment()
+
+        //SET THE DEFAULT FRAGMENT
+        //APPLY FRAGMENT TO FRAME
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_fragment, chatsFragment)
+            commit()
+        }
+
+        // ON CHATS CLICK
+        val tv_chat = findViewById<TextView>(R.id.tv_chat)
+        tv_chat.setOnClickListener {
+            //APPLY CHATS FRAGMENT TO FRAME
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fl_fragment, chatsFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        // ON PROFILE CLICK
+        val tv_profile = findViewById<TextView>(R.id.tv_profile)
+        tv_chat.setOnClickListener {
+            //APPLY PROFILE FRAGMENT TO FRAME
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fl_fragment, profileFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        // ON CONTACTS CLICK
+        val tv_contacts = findViewById<TextView>(R.id.tv_contacts)
+        tv_chat.setOnClickListener {
+            //APPLY CONTACTS FRAGMENT TO FRAME
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fl_fragment, contactsFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
 
         val userId = intent.getStringExtra(Constants.LOGGED_IN_ID)
 
