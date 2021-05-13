479d91141d396f4caff903040afb2b28c57ffae9
LaraCassandra <190218@virtualwindow.co.za>
2021-05-13 13:01:14 +0200

    Add validation for register activity

--- LinkApp/app/src/main/java/com/example/linkapp/RegisterActivity.kt
+++ LinkApp/app/src/main/java/com/example/linkapp/RegisterActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,12 +20,38 @@ class RegisterActivity : BaseActivity() {
             startActivity(intent)
         }
 
+
+        fun registerUser(){
+
+            // VARIABLES
+            val email: String = findViewById<EditText>(R.id.et_email).text.toString().trim{ it <= ' '}
+            val name: String = findViewById<EditText>(R.id.et_name).text.toString().trim{ it <= ' '}
+            val password: String = findViewById<EditText>(R.id.et_password).text.toString().trim{ it <= ' '}
+
+            // VALIDATION
+            if(email == "" || password == ""){
+                showErrorSnackBar("Please enter your email and password", true);
+            }
+            else {
+                //TODO: firebase register function
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(
+                        
+                    )
+            }
+        }
+
         val register_btn = findViewById<Button>(R.id.btn_register)
         register_btn.setOnClickListener {
-            val intent = Intent(this, OnboardingActivity::class.java)
-            startActivity(intent)
-            finish()
+            registerUser()
         }
+
+//        val register_btn = findViewById<Button>(R.id.btn_register)
+//        register_btn.setOnClickListener {
+//            val intent = Intent(this, OnboardingActivity::class.java)
+//            startActivity(intent)
+//            finish()
+//        }
 
     }
 }
