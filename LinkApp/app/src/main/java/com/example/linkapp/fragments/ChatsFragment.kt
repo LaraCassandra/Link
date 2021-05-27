9606d08801c5fdd3afaf252ab3dde6846570f647
LaraCassandra <190218@virtualwindow.co.za>
2021-05-27 13:55:56 +0200

    Add recycler view and items for chats

--- LinkApp/app/src/main/java/com/example/linkapp/fragments/ChatsFragment.kt
+++ LinkApp/app/src/main/java/com/example/linkapp/fragments/ChatsFragment.kt
@@ -1,8 +1,32 @@
 package com.example.linkapp.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.linkapp.R
+import com.example.linkapp.adapters.ChatAdapter
+import com.example.linkapp.model.ChatItem
+import kotlinx.android.synthetic.main.fragment_chats.*
 
-class ChatsFragment : Fragment(R.layout.fragment_chats){
+class ChatsFragment : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_chats, container, false)
+
+        val chatList = mutableListOf(
+            ChatItem(1, 2, "John", "Hey how are you")
+        )
+
+        //SET UP RECYCLERVIEW ADAPTER
+        rv_container.adapter = ChatAdapter(chatList, this)
+        rv_container.setHasFixedSize(true)
+
+    }
 
 }
---
+++ LinkApp/app/src/main/java/com/example/linkapp/model/ChatItem.kt
@@ -0,0 +1,8 @@
+package com.example.linkapp.model
+
+data class ChatItem (
+    val id: Int,
+    val contactId: Int,
+    val contactName: String,
+    val message: String
+)
